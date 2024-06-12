d55d511612bfd19ff08d5c655f1ab3fe6035d392
sgrcgfdd <1203494336@qq.com>
2024-06-12 09:02:56 +0800

    Update demo-basic-operations.py

--- demo-basic-operations.py
+++ demo-basic-operations.py
@@ -4,8 +4,7 @@ st.header("You need to learn some basic operations")
 st.subheader("eg, Visual Studio Code")
 st.text("It's not that hard")
 
-st.markdown("this is an image: \n \
-            ![](https://www.tjnu.edu.cn/__local/2/AE/41/C1274DD9685B10B9F814C69BEDB_84ADD00D_88D66.png)")
+
 
 
     
